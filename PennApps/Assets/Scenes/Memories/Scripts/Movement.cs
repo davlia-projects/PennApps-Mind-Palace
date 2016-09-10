57a1a63f1cc25b58fb9ed6ff15991ba871f171ae
davlia <danedaworld@gmail.com>
2016-09-10 15:38:21 -0400

    removing spheres

--- PennApps/Assets/Scenes/Memories/Scripts/Movement.cs
+++ PennApps/Assets/Scenes/Memories/Scripts/Movement.cs
@@ -5,18 +5,20 @@ public class Movement : MonoBehaviour {
     float scale;
     float offsetX;
     float offsetY;
+    float timeScale;
 	// Use this for initialization
 	void Start () {
         scale = 0.001f;
         offsetX = Random.value;
         offsetY = Random.value;
+        //timeScale
         
 	}
 	
 	public void Move()
     {
-        transform.Translate(transform.forward * Mathf.Sin(Time.time + offsetX * 2 * Mathf.PI) * scale);
-        transform.Translate(transform.right * Mathf.Cos(Time.time + offsetY * 2 * Mathf.PI) * scale);
+        transform.Translate(transform.forward * Mathf.Sin(timeScale * (Time.time + offsetX * 2 * Mathf.PI)) * scale);
+        transform.Translate(transform.right * Mathf.Cos(timeScale * (Time.time + offsetY * 2 * Mathf.PI)) * scale);
 
     }
 }
