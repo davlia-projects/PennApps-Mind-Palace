9737860433910999e02a61061fc10d44d2fd0382
Grace Huang <grace732@gmail.com>
2016-09-11 03:49:54 -0400

    yoga wee

---
+++ PennApps16/PennApps/Assets/Scenes/Main Menu/Scripts/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+    Color UNSELECTED = Color.red;
+    Color SELECTED = Color.green;
+    public string nextSceneName = null;
+    public MenuButton nextSceneMenuButton;
+
+	// Use this for initialization
+	void Start () {
+        nextSceneName = null;
+        nextSceneMenuButton = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void goToSelectedLevel()
+    {
+        if (nextSceneMenuButton != null)
+        {
+            SceneManager.LoadScene(nextSceneMenuButton.sceneName);
+            print(nextSceneMenuButton.sceneName);
+        }
+    }
+
+    public void OnEnterButton(MenuButton button)
+    {
+        if (nextSceneMenuButton != null)
+        {
+            return;
+        }
+        Renderer renderer = button.GetComponentInParent<Renderer>();
+        renderer.material.color = SELECTED;
+        nextSceneMenuButton = button;
+    }
+
+    public void OnExitButton(MenuButton button)
+    {
+        Renderer renderer = button.GetComponentInParent<Renderer>();
+        renderer.material.color = UNSELECTED;
+        nextSceneMenuButton = null;
+        return;
+    }
+}
